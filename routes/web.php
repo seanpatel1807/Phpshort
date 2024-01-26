cb8fdd1cce249dfb4a31e1c570ea5b0738936f05
seanpatel1807 <seanpatel180703@gmail.com>
2024-01-26 09:56:43 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -52,5 +52,4 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/setting', IndexController::class);
 require __DIR__.'/auth.php';
