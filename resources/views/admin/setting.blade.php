f061bf7210c0b85f30d3d3f806a629b599dc5c4e
seanpatel1807 <seanpatel180703@gmail.com>
2024-01-26 16:58:14 +0530

    changes

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -1,24 +1,18 @@
 <x-admin-layout>
-    <table class="min-w-full bg-white border border-gray-200">
-        <thead>
-            <tr>
-                <th class="py-2 px-4 border-b border-gray-500">Group</th>
-                <th class="py-2 px-4 border-b border-gray-500">Name</th>
-                <th class="py-2 px-4 border-b border-gray-500">Payload</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="bg-black min-h-screen" style="width: 180vh">
+        <form method="POST" action="" class="w-full max-w-md p-6 rounded-lg shadow-md bg-gray-800">
+            @csrf
             @foreach($groupedSettings as $group => $groupSettings)
                 @if($group === 'general')
                     @foreach($groupSettings as $setting)
-                        <tr>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $setting->group }}</td>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $setting->name }}</td>
-                            <td class="py-2 px-4 border-b border-gray-200">{{ $setting->payload }}</td>
-                        </tr>
+                        <div class="mb-4">
+                            <label for="{{ $setting->name }}" class="block text-lg font-semibold text-white">{{ $setting->name }}</label>
+                            <input type="text" id="{{ $setting->name }}" name="{{ $setting->name }}" value="{{ $setting->payload }}" placeholder="{{ $setting->payload }}" class="w-full px-4 py-2 border border-gray-700 rounded-md bg-gray-700 text-white focus:outline-none focus:border-blue-500 text-lg">
+                        </div>
                     @endforeach
                 @endif
             @endforeach
-        </tbody>
-    </table>
+            <button type="submit" class="bg-white text-black px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue text-lg">Save Changes</button>
+        </form>
+    </div>
 </x-admin-layout>
